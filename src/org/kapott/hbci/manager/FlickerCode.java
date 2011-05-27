bc10c8f5af81a69b0d706653e96af9717b4d7f7e
willuhn <willuhn>
2011-05-27 11:23:13 +0000

    @C Kommentar-Aenderungen wieder entfernt - noch nicht einchecken - sonst passt das naechste Patch ggf. nicht mehr

--- src/org/kapott/hbci/manager/FlickerCode.java
+++ src/org/kapott/hbci/manager/FlickerCode.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hbci4java/src/org/kapott/hbci/manager/FlickerCode.java,v $
- * $Revision: 1.4 $
- * $Date: 2011/05/27 11:21:38 $
+ * $Revision: 1.5 $
+ * $Date: 2011/05/27 11:23:13 $
  * $Author: willuhn $
  *
  * Copyright (c) by willuhn - software & services
@@ -60,9 +60,6 @@ public class FlickerCode
    * Die Anzahl der Bytes, in der die Laenge des Challenge steht.
    * Bei HHD 1.3 war das noch 2 Zeichen lang.
    * Ich habe keine Ahnung, woran ich erkennen kann, wenn der nur 2 Stellen lang ist.
-   * Wenn der Flicker-Code nicht in "Challenge HHDuc" uebertragen wurde
-   * sondern direkt im Freitext-Challenge, koennen wir das Problem umgehen,
-   * indem wir in clean() einfach eine "0" vorn anhaengen.
    */
   private final static int LC_LENGTH_HHD14 = 3;
   
@@ -107,9 +104,11 @@ public class FlickerCode
   public DE de3           = new DE();
   
   /**
-   * Der Rest des Codes. Mit dem koennen wir nichts anfangen
+   * Der Rest des Codes. In aller Regel sollte das ein Byte sein.
+   * Wobei das linke Halbbyte die Luhn-Pruefziffer ist und das rechte
+   * die XOR-Summe.
    */
-  public String rest      = null;
+  public String cb        = null;
 
   /**
    * ct.
@@ -143,7 +142,7 @@ public class FlickerCode
     code = this.de3.parse(code);
 
     // 4. Den Rest speichern wir hier.
-    this.rest = code.length() > 0 ? code : null;
+    this.cb = code.length() > 0 ? code : null;
   }
   
   /**
@@ -179,7 +178,9 @@ public class FlickerCode
   }
 
   /**
-   * Rendert den flickerfaehigen Code aus dem Challenge im HHD-Format.
+   * Rendert den flickerfaehigen Code aus dem Challenge im HHD 1.4-Format.
+   * Das HHD 1.3-Format wird nicht unterstuetzt. Der TAN-Generator muss also
+   * HHD 1.4-faehig sein.
    * @return der neu generierte Flicker-Code.
    */
   public String render()
@@ -245,12 +246,16 @@ public class FlickerCode
    */
   private String createXORChecksum(String payload)
   {
+    ////////////////////////////////////////////////////////////////////////////
+    // Schritt 1: Pruefziffer berechnen
     int xorsum = 0;
     for (int i=0; i<payload.length(); ++i)
     {
       xorsum ^= Integer.parseInt(Character.toString(payload.charAt(i)),16);
     }
     return toHex(xorsum,1);
+    //
+    ////////////////////////////////////////////////////////////////////////////
   }
 
   /**
@@ -320,7 +325,7 @@ public class FlickerCode
     sb.append("DE1:\n" + this.de1 + "\n");
     sb.append("DE2:\n" + this.de2 + "\n");
     sb.append("DE3:\n" + this.de3 + "\n");
-    sb.append("CB : " + this.rest + "\n");
+    sb.append("CB : " + this.cb + "\n");
     return sb.toString();
   }
   
@@ -340,9 +345,9 @@ public class FlickerCode
     if (!this.de2.equals(other.de2))             return false;
     if (!this.de3.equals(other.de3))             return false;
     
-    if (this.rest == null)
-      return (other.rest == null);
-    return this.rest.equals(other.rest);
+    if (this.cb == null)
+      return (other.cb == null);
+    return this.cb.equals(other.cb);
   }
 
   
@@ -718,14 +723,8 @@ public class FlickerCode
 
 /**********************************************************************
  * $Log: FlickerCode.java,v $
- * Revision 1.4  2011/05/27 11:21:38  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2011-05-27 11:20:36  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2011-05-27 11:19:44  willuhn
- * *** empty log message ***
+ * Revision 1.5  2011/05/27 11:23:13  willuhn
+ * @C Kommentar-Aenderungen wieder entfernt - noch nicht einchecken - sonst passt das naechste Patch ggf. nicht mehr
  *
  * Revision 1.1  2011-05-27 10:28:38  willuhn
  * @N 22-hbci4java-chiptan-opt.patch
