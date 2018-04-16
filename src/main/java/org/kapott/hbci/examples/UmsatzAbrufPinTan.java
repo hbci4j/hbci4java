6b104a1864035f32c34834f9d7eb24b28f135157
Olaf Willuhn <github@willuhn.de>
2018-04-16 09:49:44 +0200

    TAN-spezifische Callbacks im Beispielcode dokumentiert

--- src/main/java/org/kapott/hbci/examples/UmsatzAbrufPinTan.java
+++ src/main/java/org/kapott/hbci/examples/UmsatzAbrufPinTan.java
@@ -10,6 +10,7 @@ import org.kapott.hbci.GV_Result.GVRKUms;
 import org.kapott.hbci.GV_Result.GVRKUms.UmsLine;
 import org.kapott.hbci.GV_Result.GVRSaldoReq;
 import org.kapott.hbci.callback.AbstractHBCICallback;
+import org.kapott.hbci.exceptions.HBCI_Exception;
 import org.kapott.hbci.manager.BankInfo;
 import org.kapott.hbci.manager.HBCIHandler;
 import org.kapott.hbci.manager.HBCIUtils;
@@ -241,6 +242,128 @@ public class UmsatzAbrufPinTan
         case NEED_CUSTOMERID:
           retData.replace(0,retData.length(),USER);
           break;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Die folgenden Callbacks sind nur fuer die Ausfuehrung TAN-pflichtiger
+        // Geschaeftsvorfaelle bei der Verwendung des PIN/TAN-Verfahrens noetig.
+        // Z.Bsp. beim Versand einer Ueberweisung
+          
+        // HBCI4Java benoetigt die TAN per PhotoTAN-Verfahren
+        // Liefert die anzuzeigende PhotoTAN-Grafik, die mit der entsprechenden
+        // Smartphone-App der Bank fotografiert werden muss, um die TAN
+        // zu generieren. Eine Implementierung muss diese Grafik anzeigen
+        // sowie ein Eingabefeld fuer die TAN. Der Callback muss dann die vom
+        // User eingegebene TAN zurueckliefern (nachdem dieser die Grafik
+        // fotografiert und die App ihm die TAN angezeigt hat)
+        case NEED_PT_PHOTOTAN:
+          // Die Klasse "MatrixCode" kann zum Parsen der Daten verwendet werden
+          try
+          {
+            // MatrixCode code = new MatrixCode(retData.toString());
+            
+            // Liefert den Mime-Type der grafik (i.d.R. "image/png").
+            // String type = code.getMimetype();
+            
+            // Der Stream enthaelt jetzt die Binaer-Daten des Bildes
+            // InputStream stream = new ByteArrayInputStream(code.getImage());
+            
+            // .... Hier Dialog mit der Grafik anzeigen und User-Eingabe der TAN
+            // Die Variable "msg" aus der Methoden-Signatur enthaelt uebrigens
+            // den bankspezifischen Text mit den Instruktionen fuer den User.
+            // Der Text aus "msg" sollte daher im Dialog dem User angezeigt
+            // werden. 
+            String tan = null;
+            retData.replace(0,retData.length(),tan);
+          }
+          catch (Exception e)
+          {
+            throw new HBCI_Exception(e);
+          }
+
+          break;
+          
+        // HBCI4Java benoetigt den Code des verwendenden TAN-Verfahren (smsTAN,
+        // chipTAN optisch, photoTAN,...)
+        // I.d.R. ist das eine dreistellige mit "9" beginnende Ziffer
+        case NEED_PT_SECMECH:
+          
+          // Als Parameter werden die verfuegbaren TAN-Verfahren uebergeben.
+          // Der Aufbau des String ist wie folgt:
+          // <code1>:<name1>|<code2>:<name2>|...
+          // Bsp:
+          // 911:smsTAN|920:chipTAN optisch|955:photoTAN
+          // String options = retData.toString();
+          
+          // Der Callback muss den Code des zu verwendenden TAN-Verfahrens
+          // zurueckliefern
+          // In "code" muss der 3-stellige Code des vom User gemaess obigen
+          // Optionen ausgewaehlte Verfahren eingetragen werden
+          String code = "";
+          retData.replace(0,retData.length(),code);
+          break;
+          
+        // HBCI4Java benoetigt die TAN per smsTAN/chipTAN/weiteren TAN-Verfahren
+        case NEED_PT_TAN:
+          
+          // Wenn per "retData" Daten uebergeben wurden, dann enthalten diese
+          // den fuer chipTAN optisch zu verwendenden Flickercode.
+          // Falls nicht, ist es eine TAN-Abfrage, fuer die keine weiteren
+          // Parameter benoetigt werden (z.Bsp. beim smsTAN-Verfahren)
+          
+          // Die Variable "msg" aus der Methoden-Signatur enthaelt uebrigens
+          // den bankspezifischen Text mit den Instruktionen fuer den User.
+          // Der Text aus "msg" sollte daher im Dialog dem User angezeigt
+          // werden.
+          
+          String flicker = retData.toString();
+          if (flicker != null && flicker.length() > 0)
+          {
+            // Ist chipTAN optisch. Es muss ein animierter Barcode angezeigt
+            // werden. Hierfuer kann die Hilfsklasse "FlickerRenderer" verwendet
+            // werden. Diese enthalt bereits das Parsen. Es muss lediglich die
+            // Methode "paint" ueberschrieben werden.
+            // FlickerRenderer renderer = new FlickerRenderer(flicker);
+            
+            // Hier TAN-Abfrage mit dem animierten Barcode anzeigen sowie
+            // Eingabefeld fuer die TAN
+            String tan = null;
+            retData.replace(0,retData.length(),tan);
+          }
+          else
+          {
+            // Ist smsTAN, iTAN, o.ae.
+            // Dialog zur TAN-Eingabe anzeigen mit dem Text aus "msg".
+            String tan = null;
+            retData.replace(0,retData.length(),tan);
+          }
+          
+          break;
+          
+        // Beim Verfahren smsTAN ist es moeglich, mehrere Handynummern mit
+        // Aliasnamen bei der Bank zu hinterlegen. Auch wenn nur eine Handy-
+        // Nummer bei der Bank hinterlegt ist, kann es durchaus passieren,
+        // dass die Bank dennoch die Aufforderung zur Auswahl des TAN-Mediums
+        // sendet.
+        case NEED_PT_TANMEDIA:
+          
+          // Als Parameter werden die verfuegbaren TAN-Medien uebergeben.
+          // Der Aufbau des String ist wie folgt:
+          // <name1>|<name2>|...
+          // Bsp:
+          // Privathandy|Firmenhandy
+          // String options = retData.toString();
+          
+          // Der Callback muss den vom User ausgewaehlten Aliasnamen
+          // zurueckliefern. Falls "options" kein "|" enthaelt, ist davon
+          // auszugehen, dass nur eine moegliche Option existiert. In dem
+          // Fall ist keine Auswahl noetig und "retData" kann unveraendert
+          // bleiben
+          String alias = null;
+          retData.replace(0,retData.length(),alias);
+
+          break;
+        //
+        ////////////////////////////////////////////////////////////////////////
 
         // Manche Fehlermeldungen werden hier ausgegeben
         case HAVE_ERROR:
