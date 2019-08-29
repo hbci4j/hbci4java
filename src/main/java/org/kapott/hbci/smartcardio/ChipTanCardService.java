21fb55655628512b0423d34d34c28d74e8181e0d
Olaf Willuhn <github@willuhn.de>
2019-08-29 11:55:50 +0200

    Weiteren Fehler beim Finalisieren der ChipTAN-Transaktion tolerieren - sie Mail von Werner vom 29.08.2019

--- src/main/java/org/kapott/hbci/smartcardio/ChipTanCardService.java
+++ src/main/java/org/kapott/hbci/smartcardio/ChipTanCardService.java
@@ -197,6 +197,7 @@ public class ChipTanCardService extends SmartCardService
     // SECODER FINALIZE TRANSACTION (SECODER 3G)
     
     // Transaktion finalisieren
+    try
     {
       HBCIUtils.log("finalizing chipcard transaction",HBCIUtils.LOG_DEBUG);
       ByteArrayOutputStream cmd = new ByteArrayOutputStream();
@@ -214,7 +215,13 @@ public class ChipTanCardService extends SmartCardService
       // ja bereits. Wenn das finalize danach nicht unterstuetzt wird,
       // lassen wir das durchgehen.
       CommandAPDU apdu = new CommandAPDU(request);
-      this.receive(apdu,new byte[]{(byte)0x90,(byte)0x91,(byte)0x6D});
+      this.receive(apdu,new byte[]{(byte)0x90,(byte)0x91,(byte)0x6D,(byte)0x01});
+    }
+    catch (Exception e)
+    {
+        // Den Fehler tolerieren wir
+        HBCIUtils.log("error while finalizing transaction, continue nevertheless since we already have a TAN - stacktrace for debugging purpose",HBCIUtils.LOG_DEBUG);
+        HBCIUtils.log(e,HBCIUtils.LOG_DEBUG);
     }
     
     HBCIUtils.log("returning TAN",HBCIUtils.LOG_DEBUG);
