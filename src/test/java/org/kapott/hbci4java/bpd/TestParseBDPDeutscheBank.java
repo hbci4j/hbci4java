72ceab686abe8a5e8b6f7313b79ad86a658efb59
Olaf Willuhn <github@willuhn.de>
2025-10-27 10:24:13 +0100

    Support für die neue Segment-Version 2 beim Abruf und Löschen von SEPA-Daueraufträgen

---
+++ src/test/java/org/kapott/hbci4java/bpd/TestParseBDPDeutscheBank.java
@@ -0,0 +1,79 @@
+/**********************************************************************
+ *
+ * This file is part of HBCI4Java.
+ * Copyright (c) Olaf Willuhn
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **********************************************************************/
+
+package org.kapott.hbci4java.bpd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.kapott.hbci.manager.HBCIKernelImpl;
+import org.kapott.hbci.manager.MsgGen;
+import org.kapott.hbci.protocol.MSG;
+import org.kapott.hbci.protocol.factory.MSGFactory;
+import org.kapott.hbci4java.AbstractTest;
+
+/**
+ * Testet, dass das "HICDBS" (DauerSEPAList) korrekt erkannt wird.
+ */
+public class TestParseBDPDeutscheBank extends AbstractTest
+{
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void test001() throws Exception
+  {
+    try
+    {
+      final String data = getFile("bpd-deutschebank.txt");
+      final HBCIKernelImpl kernel = new HBCIKernelImpl(null,"300");
+      kernel.rawNewMsg("Synch");
+      
+      final MsgGen gen = kernel.getMsgGen();
+      final MSG msg = MSGFactory.getInstance().createMSG("SynchRes",data,data.length(),gen);
+      final Hashtable<String,String> ht = new Hashtable<String,String>();
+      msg.extractValues(ht);
+      
+      final List<String> keys = new ArrayList(ht.keySet());
+      Collections.sort(keys);
+      
+      for (String s:keys)
+      {
+        final String value = ht.get(s);
+        if (Objects.equals(value,"HICDBS"))
+        {
+          Assert.assertTrue(s.contains("DauerSEPAListPar"));
+        }
+      }
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      throw e;
+    }
+  }
+}
+
