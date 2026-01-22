b5f941c3b8012a5a330bcce18cfa2d762bdfc83b
Olaf Willuhn <github@willuhn.de>
2026-01-22 10:28:08 +0100

    Wir brauchen diese Klassen auch noch im alten Package-Namen, damit die existierenden Passports noch deserialisiert werden können.

---
+++ src/main/java/org/kapott/hbci/manager/HBCIKey.java
@@ -0,0 +1,83 @@
+/**********************************************************************
+ *
+ * This file is part of HBCI4Java.
+ * Copyright (c) 2001-2008 Stefan Palme
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **********************************************************************/
+
+package org.kapott.hbci.manager;
+
+import java.io.Serializable;
+import java.security.Key;
+
+/**
+ * Die Klasse gibt es aus Gründen der Abwärtskompatibilität doppelt, da Instanzen
+ * dieses Objekts serialisiert werden, wenn ein Passport gespeichert wird.
+ * Wir könnten sonst keine Passports mehr laden, die vor der Package-Umbenennung erstellt wurden.
+ */
+@Deprecated
+public final class HBCIKey implements Serializable
+{
+    private static final long serialVersionUID =1L;
+    
+    /** Ländercode des Schlüsselbesitzers */
+    public String country;
+    /** Bankleitzahl des Schlüsselbesitzers */
+    public String blz;
+    /** Nutzerkennung des Schlüsselbesitzers. Wenn der Schlüssel
+        einem "richtigen" Nutzer gehört, so wird hier seine HBCI-Userkennung eingestellt;
+        gehört der Schlüssel der Bank, so steht hier eine bankinterne
+        ID (u.U. die Bankleitzahl o.ä.) */
+    public String userid;
+    /** Schlüsselnummer */
+    public String num;
+    /** Schlüsselversion */
+    public String version;
+    /** kryptographische Schlüsseldaten (kann <code>null</code> sein)*/
+    public Key key;
+
+    /** Neues <code>HBCIKey</code>-Objekt erzeugen */
+    public HBCIKey()
+    {
+        // empty constructor
+    }
+
+    public HBCIKey(String country, String blz, String userid, String num, String version, Key key)
+    {
+        this.country = country;
+        this.blz = blz;
+        this.userid = userid;
+        this.num = num;
+        this.version = version;
+        this.key = key;
+    }
+    
+    @Override
+    public String toString()
+    {
+        StringBuffer ret=new StringBuffer();
+        
+        ret.append("country="+this.country);
+        ret.append(", blz="+this.blz);
+        ret.append(", userid="+this.userid);
+        ret.append(", num="+this.num);
+        ret.append(", version="+this.version);
+        ret.append(", key="+this.key);
+        
+        return ret.toString();
+    }
+}
---
+++ src/main/java/org/kapott/hbci/passport/storage/PassportData.java
@@ -0,0 +1,146 @@
+/**********************************************************************
+ *
+ * This file is part of HBCI4Java.
+ * Copyright (c) Olaf Willuhn
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **********************************************************************/
+
+package org.kapott.hbci.passport.storage;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.hbci4java.hbci.manager.HBCIKey;
+
+/**
+ * Die Klasse gibt es aus Gründen der Abwärtskompatibilität doppelt, da Instanzen
+ * dieses Objekts serialisiert werden, wenn ein Passport gespeichert wird.
+ * Wir könnten sonst keine Passports mehr laden, die vor der Package-Umbenennung erstellt wurden.
+ */
+@Deprecated
+public class PassportData implements Serializable
+{
+    // Stellt sicher, dass das Objekt auch dann noch deserialisierbar ist, wenn Attribute hinzugekommen sind.
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * Die BPD.
+     */
+    public Properties bpd = null;
+    
+    /**
+     * Die UPD.
+     */
+    public Properties upd = null;
+    
+    /**
+     * Die HBCI-Version.
+     */
+    public String hbciVersion = null;
+    
+    /**
+     * Der Laendercode.
+     */
+    public String country = null;
+    
+    /**
+     * Die BLZ.
+     */
+    public String blz = null;
+    
+    /**
+     * Der Host.
+     */
+    public String host = null;
+    
+    /**
+     * Der TCP-Port.
+     */
+    public Integer port = null;
+    
+    /**
+     * Die User-ID.
+     */
+    public String userId = null;
+    
+    /**
+     * Die System-ID.
+     */
+    public String sysId = null;
+    
+    /**
+     * Die Signatur-ID.
+     */
+    public Long sigId = null;
+    
+    /**
+     * Die Profil-Version.
+     */
+    public String profileVersion = null;
+    
+    /**
+     * Die Kunden-ID.
+     */
+    public String customerId = null;
+    
+    /**
+     * Der Transport-Filter.
+     */
+    public String filter = null;
+    
+    /**
+     * Die Liste der Zweischritt-Verfahren.
+     */
+    public List<String> twostepMechs = new ArrayList<String>();
+    
+    /**
+     * Das TAN-Verfahren.
+     */
+    public String tanMethod = null;
+
+    /**
+     * Der Signatur-Key der Bank.
+     */
+    public HBCIKey instSigKey = null;
+    
+    /**
+     * Der Verschluesselungskey der Bank.
+     */
+    public HBCIKey instEncKey = null;
+    
+    /**
+     * Der eigene Public-Signatur-Key.
+     */
+    public HBCIKey myPublicSigKey = null;
+    
+    /**
+     * Der eigene Private-Signatur-Key.
+     */
+    public HBCIKey myPrivateSigKey = null;
+    
+    /**
+     * Der eigene Public-Verschluesselungskey.
+     */
+    public HBCIKey myPublicEncKey = null;
+    
+    /**
+     * Der eigene Private-Verschluesselungskey.
+     */
+    public HBCIKey myPrivateEncKey = null;
+}
